6ad257c33ca59557e2ca4fffdd700c53bed02eb7
Gabi <gabrielpk.18@gmail.com>
2022-10-12 13:19:09 -0300

    feat: update devnet v2 addresses

--- frontend/utils/addresses.ts
+++ frontend/utils/addresses.ts
@@ -15,37 +15,37 @@ export const ADDRESSES = [
         symbol: "ETH",
         decimals: 18,
       },
-      rpcUrls: ["TBD"],
+      rpcUrls: ["https://eip-4844-alpha.optimism.io"],
     },
     addresses: {
-      NFTs: "TBD",
-      wETH: "TBD",
-      DAI: "TBD",
+      NFTs: "0x2877fd7B43d7F8B71e46c31cc12907fD94270a64",
+      wETH: "0x2828141cE50567E3864470Ab5D0f0ADB193925be",
+      DAI: "0xe31e5400f70ab1DC1010B1cB24FD97ffEfD37aA1",
     },
   },
-  {
-    network: "eip4844",
-    depleted: false,
-    disclaimer: "Faucet drips 1 ETH, 1 wETH, 5000 DAI, and 5 NFTs (ERC721).",
-    etherscanPrefix: "",
-    formattedName: "Devnet v1",
-    connectionDetails: "https://hackmd.io/@inphi/SJMXL1P6c",
-    autoconnect: {
-      chainId: "0x533",
-      chainName: "EIP-4844 Devnet v1",
-      nativeCurrency: {
-        name: "Ethereum",
-        symbol: "ETH",
-        decimals: 18,
-      },
-      rpcUrls: ["https://eip-4844.optimism.io"],
-    },
-    addresses: {
-      NFTs: "0x04F1e772E19234aCF03A8A9BC72d0308AAaFEB07",
-      wETH: "0xac6e29F8DF22Ebbd22CB089f52558760F3ba4247",
-      DAI: "0x0bD9060153E7CA86341E9B7D53df2bc0eC51B985",
-    },
-  },
+  // {
+  //   network: "eip4844",
+  //   depleted: false,
+  //   disclaimer: "Faucet drips 1 ETH, 1 wETH, 5000 DAI, and 5 NFTs (ERC721).",
+  //   etherscanPrefix: "",
+  //   formattedName: "Devnet v1",
+  //   connectionDetails: "https://hackmd.io/@inphi/SJMXL1P6c",
+  //   autoconnect: {
+  //     chainId: "0x533",
+  //     chainName: "EIP-4844 Devnet v1",
+  //     nativeCurrency: {
+  //       name: "Ethereum",
+  //       symbol: "ETH",
+  //       decimals: 18,
+  //     },
+  //     rpcUrls: ["https://eip-4844.optimism.io"],
+  //   },
+  //   addresses: {
+  //     NFTs: "0x04F1e772E19234aCF03A8A9BC72d0308AAaFEB07",
+  //     wETH: "0xac6e29F8DF22Ebbd22CB089f52558760F3ba4247",
+  //     DAI: "0x0bD9060153E7CA86341E9B7D53df2bc0eC51B985",
+  //   },
+  // },
 ];
 
 /**
